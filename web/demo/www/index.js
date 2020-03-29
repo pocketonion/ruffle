6032c59122b53f9106090d501aee0c1441280058
Sean-Thomas <sean-thomas@pocketonion.net>
2020-03-29 05:23:17 -0400

    Added dropdown for sample SWFs
    - added Rust code to stop playback of audio when a Ruffle instance is destroyed.
    - replaced static polyfill embed on web demo with JS API code.
    - added swf_lib JSON file for sample SWFs.
    - added console logs for when a Ruffle instance is created or destroyed in ruffle-player.js.
    - more minor styling changes to the web demo index page.

--- web/demo/www/index.js
+++ web/demo/www/index.js
@@ -1,10 +1,33 @@
-import { polyfill } from "../../js-src/polyfills";
+import { PublicAPI } from "../../js-src/public-api";
+import { SourceAPI } from "../../js-src/source-api";
+import { public_path } from "../../js-src/public-path";
 
-polyfill(["static-content"])
+window.RufflePlayer = PublicAPI.negotiate(window.RufflePlayer, "local", new SourceAPI("local"));
+//__webpack_public_path__ = public_path(window.RufflePlayer.config, "local");
 
-let sampleFileInput = document.getElementById("sample-file");
-if (sampleFileInput) {
-    sampleFileInput.addEventListener("change", sampleFileSelected, false);
+import("./swf_lib.json").then(({default: root_obj}) => {
+    const list = document.getElementById("sample-swfs");
+    root_obj.swfs.forEach(item => {
+        let temp = document.createElement("option");
+        temp.innerHTML = item.title;
+        temp.setAttribute("value",item.location);
+        list.appendChild(temp);
+    });
+});
+
+let ruffle;
+let player;
+let container = document.getElementById('main');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  ruffle = window.RufflePlayer.newest();
+  player = ruffle.create_player();
+  container.appendChild(player);
+});
+
+let sampleFileInput = document.getElementById("sample-swfs");
+if(sampleFileInput) {
+    sampleFileInput.addEventListener("change",sampleFileSelected,false);
 }
 
 let localFileInput = document.getElementById("local-file");
@@ -16,43 +39,46 @@ if (window.location.search && window.location.search != "") {
     let urlParams = new URLSearchParams(window.location.search);
     let url = urlParams.get("file");
     if (url && url != "") {
+        console.info(url);
         loadRemoteFile(url);
     }
 }
 
+function sampleFileSelected() {
+    let selected_value = sampleFileInput.children[sampleFileInput.selectedIndex].value;
+    if (selected_value != "none") {
+        localFileInput.value = null;
+        loadRemoteFile(selected_value);
+    }
+    else {
+        replacePlayer()
+    }
+}
+
 function localFileSelected() {
+    sampleFileInput.selectedIndex = 0;
     let file = localFileInput.files[0];
     if (file) {
         let fileReader = new FileReader();
         fileReader.onload = e => {
-            playSwf(fileReader.result);
+            player.play_swf_data(fileReader.result);
         }
         fileReader.readAsArrayBuffer(file);
-    }
-}
-
-function sampleFileSelected() {
-    if (sampleFileInput.selectedIndex <= 0) {
-        // No SWF selected.
-        return;
-    }
-
-    let file = sampleFileInput.selectedOptions[0].innerText;
-    if (file) {
-        loadRemoteFile(file);
     }
 }
 
 function loadRemoteFile(url) {
     fetch(url)
         .then(response => {
-            response.arrayBuffer().then(data => playSwf(data))
+            response.arrayBuffer().then(data => player.play_swf_data(data))
         });
 }
 
 let timestamp = 0;
 let animationHandler;
 
-function playSwf(swfData) {
-    document.getElementById("player").play_swf_data(swfData);
+function replacePlayer() {
+    document.getElementById("main").children[0].remove();
+    player = ruffle.create_player();
+    container.appendChild(player);
 }
