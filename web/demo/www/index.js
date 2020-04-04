084687e20c00ae6c367d39c12ec5bb4f3f13e33f
Sean-Thomas <sean-thomas@pocketonion.net>
2020-04-03 21:20:38 -0400

    Web Demo Page Dropdown Menu JSON Fetch Call
    - Added code to fetch remote SWF file and randomly play a selected file.
    - More changes to web demo page styling.

--- web/demo/www/index.js
+++ web/demo/www/index.js
@@ -3,35 +3,49 @@ import { SourceAPI } from "../../js-src/source-api";
 import { public_path } from "../../js-src/public-path";
 
 window.RufflePlayer = PublicAPI.negotiate(window.RufflePlayer, "local", new SourceAPI("local"));
-//__webpack_public_path__ = public_path(window.RufflePlayer.config, "local");
-
-import("./swf_lib.json").then(({default: root_obj}) => {
-    const list = document.getElementById("sample-swfs");
-    root_obj.swfs.forEach(item => {
-        let temp = document.createElement("option");
-        temp.innerHTML = item.title;
-        temp.setAttribute("value",item.location);
-        list.appendChild(temp);
-    });
-});
 
 let ruffle;
 let player;
+let jsonData;
+
 let container = document.getElementById('main');
+let author_container = document.getElementById("author-container");
+let author = document.getElementById("author");
+let sampleFileInput = document.getElementById("sample-swfs");
+let localFileInput = document.getElementById("local-file");
 
 window.addEventListener('DOMContentLoaded', (event) => {
   ruffle = window.RufflePlayer.newest();
   player = ruffle.create_player();
   player.id = "player";
   container.appendChild(player);
+  fetch("JSON_URL_GOES_HERE").then(response => {
+        if (response.ok) {
+            response.json().then( data => {
+                const list = document.getElementById("sample-swfs");
+                jsonData = data;
+                jsonData.swfs.forEach(item => {
+                    let temp = document.createElement("option");
+                    temp.innerHTML = item.title;
+                    temp.setAttribute("value",item.location);
+                    list.appendChild(temp);
+                });
+                document.getElementById("sample-swfs-container").style.display = "inline-block";
+                let rn = Math.floor(Math.random() * Math.floor(sampleFileInput.children.length));
+                sampleFileInput.selectedIndex = rn + 1;
+                loadRemoteFile(jsonData.swfs[rn].location);
+                author_container.style.display = "block";
+                author.innerHTML = jsonData.swfs[rn].author;
+                author.href = jsonData.swfs[rn].location;
+        }); 
+    }
+  });
 });
 
-let sampleFileInput = document.getElementById("sample-swfs");
 if(sampleFileInput) {
     sampleFileInput.addEventListener("change",sampleFileSelected,false);
 }
 
-let localFileInput = document.getElementById("local-file");
 if (localFileInput) {
     localFileInput.addEventListener("change", localFileSelected, false);
 }
@@ -47,9 +61,14 @@ if (window.location.search && window.location.search != "") {
 
 function sampleFileSelected() {
     let selected_value = sampleFileInput.children[sampleFileInput.selectedIndex].value;
+    let selected_index = sampleFileInput.selectedIndex - 1; //We subtract 1 here because the dropdown menu inlcudes a "None" option.
     if (selected_value != "none") {
         localFileInput.value = null;
         loadRemoteFile(selected_value);
+        
+        author_container.style.display = "block";
+        author.innerHTML = jsonData.swfs[selected_index].author;
+        author.href = jsonData.swfs[selected_index].location;
     }
     else {
         replacePlayer()
@@ -58,6 +77,9 @@ function sampleFileSelected() {
 
 function localFileSelected() {
     sampleFileInput.selectedIndex = 0;
+    author_container.style.display = "none";
+    author.innerHTML = "";
+    
     let file = localFileInput.files[0];
     if (file) {
         let fileReader = new FileReader();
@@ -83,4 +105,6 @@ function replacePlayer() {
     player = ruffle.create_player();
     player.id = "player";
     container.appendChild(player);
+    author_container.style.display = "none";
+    author.innerHTML = "";
 }
